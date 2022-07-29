1ad2cdc624d5e2fc99157e523d14bdfb907ad3b1
mignzii <48212077+mignzii@users.noreply.github.com>
2022-07-29 02:06:54 +0100

    some modif

--- screen/player.js
+++ screen/player.js
@@ -1,20 +1,20 @@
 
 import * as React from 'react';
-import { ScrollView, StyleSheet,Button  } from 'react-native';
+import { ScrollView, StyleSheet,Text,View,TouchableOpacity,Image  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Video, AVPlaybackStatus } from 'expo-av';
 import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
 import colors from '../themes/color';
 import Headerscreen from './../component/header';
-import { Text, View , TouchableOpacity , Image } from 'react-native';
+import CommentaireComponent from '../component/CommentaireComponent';
 
 export default function PlayerScreen() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
   return (
    
-    <LinearGradient colors={['#3C3C3B', '#1D1D1B']} start={{x: 0, y: 0}} end={{x: 1, y: 0}} >
+    <LinearGradient colors={['#3C3C3B', '#1D1D1B']} start={{x: 0, y: 0}}  end={{x: 1, y: 0}} >
   
     <SafeAreaView >
       <Headerscreen></Headerscreen>
@@ -25,7 +25,6 @@ export default function PlayerScreen() {
         source={{
           uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
         }}
-       
         resizeMode="contain"
         isLooping
         onPlaybackStatusUpdate={status => setStatus(() => status)}
@@ -47,8 +46,30 @@ export default function PlayerScreen() {
        </View>
        
     </View>
-     
-      
+     <View style={styles.option}>
+       <Image source={require('../assets/BOUTON_AIMER_ORANGE.png')} style={styles.img}  ></Image>
+       <Image source={require('../assets/BOUTON_FAVORI_ORANGE.png')} style={styles.img} ></Image>
+       <Image source={require('../assets/BOUTON_PANIER_ORANGE.png')} style={styles.img} ></Image>
+       <Image source={require('../assets/BOUTON_AJOUT_VIDEO_DANS_PLAYLIST_ORANGE.png')} style={styles.img} ></Image>
+       <Image source={require('../assets/BOUTON_PARTAGE_ORANGE.png')} style={styles.img} ></Image>
+     </View>
+      <View style={styles.commentaire}>
+        <View style={styles.com} >
+          <View style={{display:'flex', alignItems:'center', flexDirection:'row' , marginRight:5 }}>
+          <Text style={{color:colors.Noir, paddingRight:2, borderBottomWidth:1 , fontWeight:'600', borderBottomColor:colors.Orange }}>commentaires : </Text>
+          <Text style={{color:colors.Orange , marginRight:15}}>1546</Text>
+          </View>
+          <View style={{backgroundColor:colors.Orange, height:1 , width:'50%', marginRight:7, fontWeight:'600' }}>
+
+          </View>
+          <View>
+            <Image source={require('../assets/FLECHE_COMMENTAIRE_HAUT_NOIR.png')} style={{width:15,height:15}}></Image>
+          </View>
+        </View>
+        <View style={styles.comform}>
+        <CommentaireComponent></CommentaireComponent>
+        </View>
+      </View>
       </ScrollView>
    
     </SafeAreaView>
@@ -61,10 +82,37 @@ export default function PlayerScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor:'white'
+        backgroundColor:'white',
+        
+
      },
      video:{
       width:'100%',
       height:300
+     },
+     option:{
+      backgroundColor:'ghostwhite',
+      height:50,
+      marginHorizontal:5,
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'space-between',
+      alignItems:'center',
+      paddingHorizontal:5
+      
+     },
+     img:{
+      width:45,
+      height:45
+     },
+     commentaire:{
+      margin:10
+     },
+     com:{
+      display:'flex',
+      flexDirection:'row',
+      alignItems:'center',
+      
      }
+
 });
